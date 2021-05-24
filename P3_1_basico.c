9958e4aac07ae580c4bf622a3e797f607087879a
fernando718 <82851141+fernando718@users.noreply.github.com>
2021-05-23 19:21:14 -0500

    Create P3_1_basico.c

---
+++ P3_1_basico.c
@@ -0,0 +1,20 @@
+/*Deep3_Prog1:Basico*/
+#include <stdio.h>
+int main()
+{
+  int a, b, c;
+  printf("\n Escribe el valor de a:");
+  scanf("%d", &a);
+  printf("\n Escribe el valor de b:");
+  scanf("%d", &b);
+  printf("\n Escribe el valor de c:");
+  scanf("%d", &c);
+
+  a = a + 1;
+  b = b * 2;
+  c = c * c;
+
+  printf("\n\n Ahora el valor de a es %d", a);
+  printf("\n\n Ahora el valor de b es %d", b);
+  printf("\n\n Ahora el valor de c es %d", c);
+} 
